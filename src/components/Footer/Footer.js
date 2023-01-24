19ae99a4920db5e66bfe3507983f1c3702da7a3e
NilimaDambre <nilimaaimbrill@gmail.com>
2023-01-24 18:12:40 +0530

    changes in menu button

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Footer.css'
+import styles from './Footer.module.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,35 +8,35 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Footer() {
     return (
-        <div className='footerMain'>
-            <div className='container'>
-            <Navbar collapseOnSelect  expand="lg">
-                <Container className='navContainer'>
-                    <Navbar.Brand href="#home" className='navBrand'>WHAT2EAT</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav" className='navlinks'>
-                        <Nav className='gapBetween'>
-                            <Nav.Link className='active' href="#Home">Home</Nav.Link>
-                            <Nav.Link href="#About">About</Nav.Link>
-                            <Nav.Link href="#Menu">Menu</Nav.Link>
-                            <Nav.Link href="#Blog">Blog</Nav.Link>
-                            <Nav.Link href="#Contact">Contact</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                        <div className='d-flex searchShopIconMain'>
-                            <ul className='searchShopIcon'>
-                                <li className='searchShopIcon1'><FacebookIcon /></li>
-                                <li className='searchShopIcon1'><InstagramIcon /></li>
+        <div className={styles.footerMain}>
+            <Container>
+                <Navbar collapseOnSelect expand="lg"  variant="dark">
+                    <Container className={styles.navContainer}>
+                        <Navbar.Brand href="#home" className={styles.navBrand}>WHAT2EAT</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav"  className={styles.toggleButton}  />
+                        <Navbar.Collapse id="responsive-navbar-nav" className={styles.navlinks}>
+                            <Nav className={styles.gapBetween}>
+                                <Nav.Link className={styles.active} href="#Home">Home</Nav.Link>
+                                <Nav.Link href="#About">About</Nav.Link>
+                                <Nav.Link href="#Menu">Menu</Nav.Link>
+                                <Nav.Link href="#Blog">Blog</Nav.Link>
+                                <Nav.Link href="#Contact">Contact</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                        <div className={styles.searchShopIconMain}>
+                            <ul className={styles.searchShopIcon}>
+                                <li className={styles.searchShopIcon1}><FacebookIcon /></li>
+                                <li className={styles.searchShopIcon1}><InstagramIcon /></li>
                             </ul>
                         </div>
-                </Container>
-            </Navbar>
-                <hr style={{ color: "white", border: "1px solid white" }} />
-                <div className='copyright'>
+                    </Container>
+                </Navbar>
+                <hr className={styles.horizontalLine}/>
+                <div className={styles.copyright}>
                     <p>Copyright @2021 What2Eat</p>
                 </div>
 
-            </div>
+            </Container>
         </div>
     )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,32 @@
 import React from 'react'
-import './Header.css'
+import styles from './Header.module.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 
+
 function Header() {
     return (
-        <div className='headerMain'>
-            <Navbar collapseOnSelect  expand="md">
-                <Container className='navContainer'>
-                    <Navbar.Brand href="#home" className='navBrand'>WHAT2EAT</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav" className='navlinks'>
-                        <Nav className='gapBetween'>
-                            <Nav.Link className='active' href="#Home">Home</Nav.Link>
+        <div className={styles.headerMain}>
+            <Navbar collapseOnSelect  expand="md" variant="dark">
+                <Container className={styles.navContainer}>
+                    <Navbar.Brand href="#home" className={styles.navBrand}>WHAT2EAT</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" className={styles.toggleButton} />
+                    <Navbar.Collapse id="responsive-navbar-nav" className={styles.navlinks}>
+                        <Nav className={styles.gapBetween}>
+                            <Nav.Link className={styles.active} href="#Home">Home</Nav.Link>
                             <Nav.Link href="#About">About</Nav.Link>
                             <Nav.Link href="#Menu">Menu</Nav.Link>
                             <Nav.Link href="#Blog">Blog</Nav.Link>
                             <Nav.Link href="#Contact">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
-                        <div className='d-flex searchShopIconMain'>
-                            <ul className='searchShopIcon'>
-                                <li className='searchShopIcon1'><SearchIcon /></li>
-                                <li className='searchShopIcon1'><ShoppingBagIcon /></li>
+                        <div className={styles.searchShopIconMain}>
+                            <ul className={styles.searchShopIcon}>
+                                <li className={styles.searchShopIcon1}><SearchIcon /></li>
+                                <li className={styles.searchShopIcon1}><ShoppingBagIcon /></li>
                             </ul>
                         </div>
                 </Container>
